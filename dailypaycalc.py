dd1b17c9b9d462ff32db435c4e4ebdae942a70a5
JamieVic <4652839+JamieVic@users.noreply.github.com>
2021-03-13 20:20:55 +1100

    Added try/except for dateStr
    Changed hourStr and payrateStr to initial input variables to take float values

--- dailypaycalc.py
+++ dailypaycalc.py
@@ -3,22 +3,21 @@ import datetime
 saturdayRate = 0.5
 sundayRate = 2
 
-hoursStr = input("Hours Worked: ")
-payrateStr = input("Hourly Payrate: ")
-dateStr = input("Enter Date (DD/MM/YYYY): ")
-
-day, month, year = dateStr.split("/")
-dateInt = datetime.date(int(year), int(month), int(day))
-weekday = dateInt.strftime("%A")
-
 try:
-    hours = float(hoursStr)
-    payrate = float(payrateStr)
-    if weekday == "Saturday":
-        print(hours * (payrate * saturdayRate))
-    elif weekday == "Sunday":
-        print(hours * (payrate * sundayRate))
-    else:
-        print(hours * payrate)
+    hours = float(input("Hours Worked: "))
+    payrate = float(input("Hourly Payrate: "))
+    try:
+        dateStr = input("Enter Date (DD/MM/YYYY): ")
+        day, month, year = dateStr.split("/")
+        dateInt = datetime.date(int(year), int(month), int(day))
+        weekday = dateInt.strftime("%A")
+        if weekday == "Saturday":
+            print(hours * (payrate * saturdayRate))
+        elif weekday == "Sunday":
+            print(hours * (payrate * sundayRate))
+        else:
+            print(hours * payrate)
+    except:
+        print("Invalid date entered.")
 except:
     print("Invalid character entered.")
